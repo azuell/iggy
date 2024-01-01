a80775d86ca6061d5032ece20e16f62f3f612cfd
azuell <86937359+azuell@users.noreply.github.com>
2024-01-01 22:34:25 +1100

    Add basic Iggy mouse eating

--- script.js
+++ script.js
@@ -6,16 +6,21 @@ var pageHeight = document.body.offsetHeight;
 // Get elements
 const eyes = document.querySelectorAll('.eyeball');
 const iggy = document.getElementById('iggy');
+const cursor = document.getElementById('cursor');
 
 // Iggy position
 var iggyWidth = iggy.getBoundingClientRect().width, iggyHeight = iggy.getBoundingClientRect().height;
 var iggyX = iggy.getBoundingClientRect().left + iggyWidth / 2, iggyY = iggy.getBoundingClientRect().top + iggyHeight / 2;
+var iggyBox = iggy.getBoundingClientRect();
 
 // Initial mouse position
 var x = pageWidth / 2, y = pageHeight / 2;
 
 // Both mouse and touch screen
 const events = ["mousemove", "touchmove"];
+
+// Iggy mode
+var mouseEaten = false;
 
 // Check for touch screen
 function isTouchDevice() {
@@ -29,35 +34,76 @@ function isTouchDevice() {
 
 // Listen for cursor/touch movement
 events.forEach((eventType) => {
-    document.body.addEventListener(eventType, (event) => {
-        // Update page size
-        pageWidth = document.body.offsetWidth;
-        pageHeight = document.body.offsetHeight;
+  document.body.addEventListener(eventType, (event) => {
 
-        // Cursor position from top left
-        x = !isTouchDevice() ? event.clientX : event.touches[0].clientX;
-        y = !isTouchDevice() ? event.clientY : event.touches[0].clientY;
+    // Update page size
+    pageWidth = document.body.offsetWidth;
+    pageHeight = document.body.offsetHeight;
 
-        // Move Iggy
-        if ((x > iggyWidth/2) & (x < pageWidth-(iggyWidth/2)) & (y > iggyHeight/2) & (y < pageHeight-(iggyHeight/2))) {
-            iggy.animate({left: `${x}px`, 
-                          top: `${y}px`,
-                          
-                          }, 
-                         {duration: 30000,  
-                          fill: "forwards"});
-            iggy.style.animationPlayState = "running";
-        }
+    // Cursor position from top left
+    x = !isTouchDevice() ? event.clientX : event.touches[0].clientX;
+    y = !isTouchDevice() ? event.clientY : event.touches[0].clientY;
 
-        eyes.forEach((eye) => {
-            // Calculate eye movement
-            var eyeMoveX = (eye.clientWidth * x / window.innerWidth) - (eye.clientWidth / 2);
-            var eyeMoveY = (eye.clientWidth * y / window.innerHeight) - (eye.clientHeight / 2);
+    // Move Iggy
+    if ((x > iggyWidth/2) 
+        & (x < pageWidth-(iggyWidth/2)) 
+        & (y > iggyHeight/2) 
+        & (y < pageHeight-(iggyHeight/2))) {
+          iggy.animate({left: `${x}px`, 
+                      top: `${y}px`}, 
+                      {duration: 30000,  
+                      fill: "forwards"});
+          iggy.style.animationPlayState = "running";
+    }
+        
+    // Move eyeballs
+    // Currently uses page size - probably just want to use angle and distance from mouse more instead
+    eyes.forEach((eye) => {
+      // Calculate eye movement
+      var eyeMoveX = (eye.clientWidth * x / window.innerWidth) - (eye.clientWidth / 2);
+      var eyeMoveY = (eye.clientWidth * y / window.innerHeight) - (eye.clientHeight / 2);
 
-            // Move eye
-            eye.style.transform = `translate(${eyeMoveX}px, ${eyeMoveY}px)`;
-        });
+      // Move eye
+      eye.style.transform = `translate(${eyeMoveX}px, ${eyeMoveY}px)`;
     });
+  });
 });
+
+setInterval(function() {
+  // Iggy mouse eating functionality
+
+  // Only update iggyBox if mouse isn't eaten, otherwise bounding box is 0
+  if (!mouseEaten) {
+    iggyBox = iggy.getBoundingClientRect();
+  }
+
+  // Check if cursor is within iggy bounding box
+  if ((iggyBox.left < x) 
+      & (x < iggyBox.left + iggyWidth)
+      & (iggyBox.top < y)
+      & (y < iggyBox.top + iggyHeight)) {
+      // Iggy eat cursor
+      mouseEaten = true;
+    } else {
+      // Normal cursor
+      mouseEaten = false;
+    }
+  
+
+  // Display correlated Iggy and cursor
+  if (mouseEaten) {
+      // Iggy eat cursor
+      document.body.style.cursor = "none";
+      iggy.style.display = "none";
+      cursor.style.display = "flex";
+      cursor.style.top = `${y}px`;
+      cursor.style.left = `${x}px`; 
+  } else {
+      // Normal cursor
+      document.body.style.cursor = "auto";
+      cursor.style.display = "none";
+      iggy.style.display = "flex";
+  }
+}, 10);
 
 
